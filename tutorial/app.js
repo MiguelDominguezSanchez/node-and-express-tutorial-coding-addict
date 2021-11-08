2f04d3d1ccd13a03419aff8937ee507833d15aa9
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-11-08 18:56:24 +0100

    Streams - Additional Info

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,15 +1,17 @@
-const http = require('http')
+const { createReadStream } = require('fs')
 
-// const server = http.createServer((req, res) => {
-// 	res.end('Welcome')
-// })
-
-// Using Event Emitter API
-const server = http.createServer()
-// emits request event
-// subscribe to it / listen for it / respond to it
-server.on('request', (req, res) => {
-	res.end('Welcome')
+const stream = createReadStream('../content/big.txt', {
+	highWatermark: 90000,
+	encoding: 'utf8',
 })
 
-server.listen(5000)
+// default 64kb
+// last buffer – remainder
+// highWatermark – control size
+// const stream = createReadStream('./content/big.txt', { highWatermark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+
+stream.on('data', (result) => {
+	console.log(result)
+})
+stream.on('error', (err) => console.log(err))
